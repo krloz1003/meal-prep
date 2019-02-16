4dcac0e41cd10ad5cc5705176b89d33f23cc56ca
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-02-15 22:17:57 -0600

    Instalar vue, crear componentes con ayuda de vuetify (SPA)

---
+++ src/components/HomePlans.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-container grid-list-lg>
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">Available Meal Plans</v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">KETO</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Keto</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">PALEO</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Paleo</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img src="https://images.unsplash.com/photo-1453831362806-3d5577f014a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=738&q=80" height="500px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text">VEGAN</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Vegan</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomePlans'
+};
+</script>
+
+<style scoped>
+</style>
