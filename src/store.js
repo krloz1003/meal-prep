42e936ab8339c81221b02254385539a003218bc5
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-02-18 20:25:39 -0600

    Llamando acciones a la tienda Vuex

--- src/store.js
+++ src/store.js
@@ -1,10 +1,37 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {}
+    state: {
+        recipes: [],
+        apiUrl: 'https://api.edamam.com/search'
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan){
+            console.log(state.apiUrl);
+            try {
+                // Para que funcionara se cambió ${state.apiUrl} por state.apiUrl 
+                let response = await axios.get(state.apiUrl, {
+                    params: {
+                        q: plan,
+                        app_id: 'c0075690',
+                        app_key:'4c24f0411b2319368f3963f81f6e1bbc',
+                        from: 0,
+                        to: 9
+                    }   
+                });
+                commit('setRecipes', response.data.hits);
+            } catch (error) {
+                commit('setRecipes', []);
+            }
+        }
+    }
 });
